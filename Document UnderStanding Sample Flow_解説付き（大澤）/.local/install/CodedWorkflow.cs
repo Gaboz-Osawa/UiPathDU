1e94041feb504ad05e65e3b1113fcab6e82b363b
Gaboz-Osawa <n-osawa@powersolutions.co.jp>
2024-01-30 21:12:11 +0900

    2024/1/30 Commit
    2024/1/30 Commit

---
+++ Document UnderStanding Sample Flow_解説付き（大澤）/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace DocumentUnderStandingSampleFlow
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
